eef91c9c19814e36db15c9d6bcef42fce11e0156
skywalkerrs <rs05496321786@hotmail.com>
2019-09-04 11:08:03 -0300

    f commit

---
+++ utils/csvToJSON/csvToJson.py
@@ -0,0 +1,13 @@
+import csv, json
+csvFilePath = "test.csv"
+
+ #reading csv and adding data to dictionary
+data = {}
+with open(csvFilePath) as csvFile:
+	csvReader = csv.DictReader(csvFile)
+		
+	for csvRow in csvReader:
+		date = csvRow['Date']
+		data[date] = csvRow #write to json file
+with open('jsonFilePath.json', 'w') as jsonFile:
+	jsonFile.write(json.dumps(data, indent=4))
---
+++ utils/splitJSON/scc.py
@@ -0,0 +1,15 @@
+import json
+
+with open('south.json') as json_file:
+    data = json.load(json_file)
+   
+    lista =  []
+    for p in data['utterances']:
+    	lista.append(p)
+
+for i in lista:
+	intent = (i['intent'])
+	if 'LocalGov.Waste' in intent or 'LocalGov.Bin' in intent :
+		#print(intent)
+		parsed = i
+		print(json.dumps(parsed, indent = 4, sort_keys = True))
